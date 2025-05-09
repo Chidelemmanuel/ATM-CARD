9bdedc1c6dee5a9a8cd2a6ff821cfed44d19e479
emmanuelozochi2019@gmail.com <emmanuelozochi2019@gmail.com>
2025-05-09 11:37:42 +0100

    eye slash icon added

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 'use strict'
 
 const icon = document.querySelector('#icon');
+const hide = document.querySelector('.number')
+
 
 icon.addEventListener('click', function(e) {
     e.preventDefault();
-    const hide = document.querySelector('.number')
+    // hide.textContent = '1234 **** **** 4567';
+
+    // if (icon === hide) {
+    //     console.log('yes');
+    // } else {
+    //     console.log ('No')
+    // }
+
+    let isVisible = true;
+
+  if (isVisible) {
     hide.textContent = '1234 **** **** 4567';
+    icon.classList.remove('fa-eye');
+    icon.classList.add('fa-eye-slash');
+  } else {
+    hide.textContent = '1234 5678 9012 3456';
+    icon.classList.remove('fa-eye-slash');
+    icon.classList.add('fa-eye');
+  }
+
+  isVisible = !isVisible;
 }); 
